69e01e5be3a48b22938bed3bba8c9a466544e75b
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-26 22:02:59 -0400

    add stock search

--- controllers/stock.js
+++ controllers/stock.js
@@ -1,5 +1,22 @@
+const finnhub = require('finnhub');
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = process.env.FINHUB_API_KEY;
+
+
+const finnhubClient = new finnhub.DefaultApi()
+
 module.exports = {
-  search, 
-  get, 
+  search
+}
+
+async function search(req, res) {
+  finnhubClient.quote(req.params.symbol, (error, data, response) => {
+    if(error){
+      res.status(error.status).send(error.response.text);
+    } else {
+      res.status(200).send(data)
+    }
+  });
 }
 
--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const logger = require('morgan');
 
 const app = express();
 
+require('dotenv').config();
+require('./config/database');
+stockRouter = require("./routes/api/stock");
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use('/stock', stockRouter);
 // app.get('/*', function(req, res) {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
