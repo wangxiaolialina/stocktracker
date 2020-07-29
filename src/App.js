487708ebacc58f9bb6f46f505841e639290a6f97
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-28 20:41:16 -0400

    add signup and sign in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import SignUp from './components/SignupForm/Signup';
 import SignInSide from './components/SignInSide/SignInSide';
+import HomePage from './Pages/HomePage/HomePage'
 
-function App() {
-  return (
-    <SignInSide />
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+      watchlist: [],
+      // Initialize user if there's a token, otherwise null
+      // user: userService.getUser()
+    };
+
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() => 
+              <HomePage
+              />
+            }/>
+            <Route exact path='/signup' render={() => 
+              <SignUp
+              />
+            }/>
+            <Route exact path='/login' render={() => 
+              <SignInSide
+              />
+            }/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
