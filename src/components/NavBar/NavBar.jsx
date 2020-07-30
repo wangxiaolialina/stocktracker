5adb7474e61d56fd01b016290c7d68356c902224
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-30 09:58:50 -0400

    differentiate sign in/sign out feature showed on nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,22 +5,36 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 
-const NavBar = (props) => (
-  <div>
-  <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">Stock Tracker</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Watchlist</Nav.Link>
-    </Nav>
-    <Form inline>
-      <Nav.Link href="/login">Login</Nav.Link>
-      <Nav.Link href="/signup">Signup</Nav.Link>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
-  </div>)
+const NavBar = (props) => {
+  const rightContent = props.user ?
+    <div>
+      <Form inline>
+        <Nav.Link href="/watchlist">{props.user.name}</Nav.Link>
+        <Nav.Link href="" onClick={props.handleLogout}>Sign Out</Nav.Link>
+      </Form>
+    </div>
+    :
+    <div>
+      <Form inline>
+        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/signup">Signup</Nav.Link>
+      </Form>
+    </div>;
 
+  return (
+    <div>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">Stock Tracker</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="/watchlist">Watchlist</Nav.Link>
+        </Nav>
+          {rightContent}
+      </Navbar.Collapse>
+    </Navbar>
+    </div>)
+
+}
 export default NavBar;
