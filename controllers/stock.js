2dde635032369c109aed9caad9916dd377ac1f7b
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-30 11:19:51 -0400

    add search news route on backend

--- controllers/stock.js
+++ controllers/stock.js
@@ -7,7 +7,9 @@ api_key.apiKey = process.env.FINHUB_API_KEY;
 const finnhubClient = new finnhub.DefaultApi()
 
 module.exports = {
-  search
+  search,
+  searchNews
+
 }
 
 async function search(req, res) {
@@ -20,3 +22,16 @@ async function search(req, res) {
   });
 }
 
+async function searchNews(req, res) {
+  let currentDate = new Date().toJSON().slice(0,10);
+  let fiveDaysago = new Date();
+  fiveDaysago.setDate(fiveDaysago.getDate()-5);
+  let fromDate = fiveDaysago.toJSON().slice(0,10);
+  finnhubClient.companyNews(req.params.symbol, fromDate, currentDate, (error, data, response) => {
+    if (error) {
+      res.status(error.status).send(error.response.text);
+    } else {
+      res.status(200).send(data)
+    }
+});
+}
--- routes/api/stock.js
+++ routes/api/stock.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 stockCtrl = require('../../controllers/stock');
 
+
+router.get('/companynews/:symbol',stockCtrl.searchNews);
 router.get('/:symbol',stockCtrl.search);
+
 
 module.exports = router;
