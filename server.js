85cf806f30cc52c63b88bec54b90c7aa99820752
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-30 23:09:04 -0400

    client side routing

--- server.js
+++ server.js
@@ -24,11 +24,8 @@ app.use('/api/watchlist', require("./routes/api/watchlist"));
 const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
